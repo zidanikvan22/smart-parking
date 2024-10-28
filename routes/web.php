7a626574d3a49446aadfdca6382981d7bce8490d
Adib-F <fakhrimuhammadadib@gmail.com>
2024-10-28 17:05:21 +0700

    Create: Add routing for all necessary files

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,37 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\RealTimeController;
+use App\Http\Controllers\QrCodeController;
+use App\Http\Controllers\AnalysisController;
+use App\Http\Controllers\ChangePasswordController;
+use App\Http\Controllers\AdminDashboardController;
+use App\Http\Controllers\AdminUserController;
+use App\Http\Controllers\AdminZonaController;
+use App\Http\Controllers\AdminAnalysisController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Autentikasi
+Route::get('/', [AuthController::class, 'login'])->name('login');
+Route::get('/registrasi', [AuthController::class, 'registrasi'])->name('registrasi');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-Route::get('/tes', function () {
-    return view('tes');
+//Dashboard
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+//Fitur Utama
+Route::get('/real-time', [RealTimeController::class, 'index'])->name('real-time');
+Route::get('/qr-code', [QrCodeController::class ,'index'])->name('qr-code');
+Route::get('/analysis', [AnalysisController::class ,'index'])->name('analysis');
+
+//ubah kata sandi
+Route::get('/change-password', [ChangePasswordController::class ,'index'])->name('ubah-sandi');
+
+//admin
+Route::prefix('admin')->group(function () {
+    Route::get('/dashboard', [AdminDashboardController::class, 'index'])->name('admin-dashboard');
+    Route::get('/users', [AdminUserController::class, 'index'])->name('admin-users');
+    Route::get('/zona', [AdminZonaController::class, 'index'])->name('admin-zona');
+    Route::get('/analysis', [AdminAnalysisController::class, 'index'])->name('admin-analysis');
 });
