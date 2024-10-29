d9773d544f281d2191b4610e38d3ce5a280f1ceb
Adib-F <fakhrimuhammadadib@gmail.com>
2024-10-29 18:15:04 +0700

    Update: Move admin header component to its own file

--- resources/views/layout/mainAdmin.blade.php
+++ resources/views/layout/mainAdmin.blade.php
@@ -36,65 +36,8 @@
         <!-- Main Content -->
         <div class="flex flex-col flex-grow">
             <!-- Header -->
-            <div class="grid grid-cols-5 p-5">
-                <!-- Title -->
-                <div class="flex items-center justify-center col-span-2">
-                    <span class="text-xl font-semibold">{{ $title }}</span>
-                </div>
-
-                <!-- Search Bar -->
-                <div class="flex items-center justify-center w-full col-span-2">
-                    <div class="relative w-full">
-                        <input type="text"
-                            class="block w-full px-4 py-3 text-sm bg-gray-100 border border-transparent rounded-full peer pl-11 focus:border-blue-500 focus:ring-blue-500 dark:bg-neutral-700 dark:border-transparent dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
-                            placeholder="Cari">
-                        <div
-                            class="absolute inset-y-0 left-0 flex items-center pl-4 pointer-events-none peer-disabled:opacity-50 peer-disabled:pointer-events-none">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#000000"
-                                viewBox="0 0 256 256">
-                                <path
-                                    d="M229.66,218.34l-50.07-50.06a88.11,88.11,0,1,0-11.31,11.31l50.06,50.07a8,8,0,0,0,11.32-11.32ZM40,112a72,72,0,1,1,72,72A72.08,72.08,0,0,1,40,112Z">
-                                </path>
-                            </svg>
-                        </div>
-                    </div>
-                </div>
-
-                <!-- Account -->
-                <div class="flex justify-end col-span-1">
-                    <div class="relative inline-flex hs-dropdown">
-                        <button id="hs-dropdown-custom-trigger" type="button"
-                            class="inline-flex items-center py-1 text-sm font-medium text-gray-800 bg-white border border-gray-200 rounded-full shadow-sm hs-dropdown-toggle ps-1 pe-3 gap-x-2 hover:bg-gray-50 focus:outline-none focus:bg-gray-50 dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800 dark:focus:bg-neutral-800"
-                            aria-haspopup="menu" aria-expanded="false" aria-label="Dropdown">
-                            <img class="w-8 h-auto rounded-full"
-                                src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=300&h=300&q=80"
-                                alt="Avatar">
-                            <span class="text-gray-600 font-medium truncate max-w-[7.5rem] dark:text-neutral-400">Jane
-                                Doe</span>
-                            <svg class="hs-dropdown-open:rotate-180 size-4" xmlns="http://www.w3.org/2000/svg"
-                                width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                                stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                                <path d="m6 9 6 6 6-6" />
-                            </svg>
-                        </button>
-                        <div class="hs-dropdown-menu transition-[opacity,margin] duration hs-dropdown-open:opacity-100 opacity-0 z-10 hidden min-w-60 bg-white shadow-md rounded-lg mt-2 dark:bg-neutral-800 dark:border dark:border-neutral-700"
-                            role="menu" aria-orientation="vertical" aria-labelledby="hs-dropdown-custom-trigger">
-                            <div class="p-1 space-y-0.5">
-                                <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700"
-                                    href="/pengaturan">
-                                    Pengaturan
-                                </a>
-                            </div>
-                            <div class="p-1 space-y-0.5">
-                                <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700"
-                                    aria-haspopup="dialog" aria-expanded="false"
-                                    aria-controls="hs-vertically-centered-scrollable-modal" data-hs-overlay="#modal">
-                                    Logout
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            <div class="grid grid-cols-5 p-5 border-b-4">
+                @include('component/headerAdmin')
             </div>
 
             <!-- Content -->
