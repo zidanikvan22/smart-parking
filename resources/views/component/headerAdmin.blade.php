b9d0c227e4e9548a5973fb756e560e7cccaedd70
Adib-F <fakhrimuhammadadib@gmail.com>
2024-11-05 08:24:37 +0700

    Update: Minor changes

--- resources/views/component/headerAdmin.blade.php
+++ resources/views/component/headerAdmin.blade.php
@@ -9,7 +9,7 @@
 
 <!-- Logout -->
 <div class="flex justify-end w-full col-span-3">
-    <button class="rounded-full hover:px-5 hover:bg-blue-200" data-modal-target="popup-modal" data-modal-toggle="popup-modal" type="button">
+    <button class="transition-all delay-75 rounded-full hover:px-5 hover:bg-blue-200" data-modal-target="popup-modal" data-modal-toggle="popup-modal" type="button">
         <div class="relative inline-flex items-center">
             <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256"><path d="M120,216a8,8,0,0,1-8,8H48a8,8,0,0,1-8-8V40a8,8,0,0,1,8-8h64a8,8,0,0,1,0,16H56V208h56A8,8,0,0,1,120,216Zm109.66-93.66-40-40a8,8,0,0,0-11.32,11.32L204.69,120H112a8,8,0,0,0,0,16h92.69l-26.35,26.34a8,8,0,0,0,11.32,11.32l40-40A8,8,0,0,0,229.66,122.34Z"></path></svg>
             <p class="pl-2 pr-2 text-md">Logout</p>
