67915dab4495db22a5763fa9445fe23a017469f6
Adib-F <fakhrimuhammadadib@gmail.com>
2024-11-11 15:17:42 +0700

    Create: Mainuser template to user page structure

---
+++ resources/views/layout/mainUser.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="{{ asset('img/logoSIPPP.jpg')}}" type="image/x-icon">
+    <title>{{ $title }}</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900&display=swap"
+        rel="stylesheet">
+    @vite('resources/css/app.css')
+</head>
+
+<body class="min-h-screen font-poppins bg-gradient-to-b from-blue-500 to-white">
+    @yield('main')
+</body>
+
+</html>
