31870384f74b457e4ae6c9b0bdb474072ce85c7e
Adib-F <fakhrimuhammadadib@gmail.com>
2024-11-16 22:39:26 +0700

    Update: User detail modals based on selected user

--- resources/views/admin/manageUsers.blade.php
+++ resources/views/admin/manageUsers.blade.php
@@ -54,7 +54,8 @@
                     <td class="p-3 text-sm border border-black">Mobil</td> -->
                     <td class="p-3 text-sm border border-black">{{ $pengguna->no_plat }}</td>
                     <td class="p-3 text-sm border border-black">
-                        <button data-modal-target="small-modal" data-modal-toggle="small-modal"
+                        <button data-modal-target="small-modal{{ $pengguna->id_pengguna }}"
+                                data-modal-toggle="small-modal{{ $pengguna->id_pengguna }}"
                         class="px-2 py-1 text-xs bg-red-400 rounded-lg hover:bg-blue-200">Detail</button>
                         <button data-modal-target="hapus-modal" data-modal-toggle="hapus-modal"
                         class="px-2 py-1 text-xs bg-red-400 rounded-lg hover:bg-blue-200">Hapus</button>
@@ -103,19 +104,19 @@
 </div>
 
 <!-- Small Modal -->
-<div id="small-modal" tabindex="-1"
+@foreach ($penggunas as $pengguna )
+
+<div id="small-modal{{ $pengguna->id_pengguna }}" tabindex="-1"
     class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
     <div class="relative w-full max-w-md max-h-full">
-        <!-- Modal content -->
         <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-            <!-- Modal header -->
             <div class="flex items-center justify-between p-4 border-b rounded-t md:p-5 dark:border-gray-600">
                 <h3 class="text-xl font-medium text-gray-900 dark:text-white">
                     Data Diri & Kendaraan Pengguna
                 </h3>
-                <button type="button"
+                <button type="submit"
                     class="inline-flex items-center justify-center w-8 h-8 text-sm text-gray-400 bg-transparent rounded-lg hover:bg-gray-200 hover:text-gray-900 ms-auto dark:hover:bg-gray-600 dark:hover:text-white"
-                    data-modal-hide="small-modal">
+                    data-modal-hide="small-modal{{ $pengguna->id_pengguna }}">
                     <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
                         viewBox="0 0 14 14">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -124,38 +125,26 @@
                     <span class="sr-only">Close modal</span>
                 </button>
             </div>
-            <!-- Modal body -->
             <div class="flex items-center p-4 space-x-4 md:p-5">
-                <!-- Profile Image -->
-                <img src="{{ asset('img/user.jpg') }}" alt="User Profile" class="object-cover w-48 h-56 rounded-md -pt-5">
-
-                <!-- User Information -->
+                <img src="{{ asset('storage/' . $pengguna->foto_profile) }}" alt="User Profile" class="object-cover w-48 h-56 rounded-md -pt-5">
                 <div class="space-y-1">
                     <div class="text-base font-semibold text-gray-900 dark:text-white">Email</div>
-                    <div class="text-base text-gray-500 dark:text-gray-400">: cristiano@gmail.com</div>
+                    <div class="text-base text-gray-500 dark:text-gray-400">: {{ $pengguna->email }}</div>
 
                     <div class="text-base font-semibold text-gray-900 dark:text-white">Nama Pengguna</div>
-                    <div class="text-base text-gray-500 dark:text-gray-400">: Cristiano Ronaldo El Speed</div>
+                    <div class="text-base text-gray-500 dark:text-gray-400">: {{ $pengguna->nama }}</div>
 
                     <div class="text-base font-semibold text-gray-900 dark:text-white">Jenis Kendaraan</div>
-                    <div class="text-base text-gray-500 dark:text-gray-400">: Mobil</div>
+                    <div class="text-base text-gray-500 dark:text-gray-400">: {{ $pengguna->jenis_kendaraan }}</div>
 
                     <div class="text-base font-semibold text-gray-900 dark:text-white">Plat Kendaraan</div>
-                    <div class="text-base text-gray-500 dark:text-gray-400">: BP 0770 KU</div>
+                    <div class="text-base text-gray-500 dark:text-gray-400">: {{ $pengguna->no_plat }}</div>
                 </div>
             </div>
-            <!-- Modal footer
-            <div class="flex items-center p-4 border-t border-gray-200 rounded-b md:p-5 dark:border-gray-600">
-                <button data-modal-hide="small-modal" type="button"
-                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">I
-                    accept</button>
-                <button data-modal-hide="small-modal" type="button"
-                    class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Decline</button>
-            </div> -->
         </div>
     </div>
 </div>
-
+@endforeach
 
 
 @endsection
