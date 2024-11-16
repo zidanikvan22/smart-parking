4035134d57745f44719599a807600612e89e8403
Adib-F <fakhrimuhammadadib@gmail.com>
2024-11-16 19:44:37 +0700

    Update: Display user data with pagination

--- app/Http/Controllers/AdminUserController.php
+++ app/Http/Controllers/AdminUserController.php
@@ -2,14 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AdminUserController extends Controller
 {
     public function index()
     {
+        $users = User::query()->paginate(5);
         return view('admin.manageUsers', [
-            "title" => "ManageUsers"
+            "title" => "ManageUsers",
+            "penggunas" => $users
         ]);
     }
 }
--- resources/views/admin/manageUsers.blade.php
+++ resources/views/admin/manageUsers.blade.php
@@ -11,7 +11,7 @@
         <label for="default-search"
             class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
         <div class="relative">
-            <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+            <div class="absolute inset-y-0 flex items-center pointer-events-none start-0 ps-3">
                 <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
                     xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -19,7 +19,7 @@
                 </svg>
             </div>
             <input type="search" id="default-search"
-                class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                class="block w-full p-4 text-sm text-gray-900 border border-gray-300 rounded-lg ps-10 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="Search Nama pengguna, Plat nomor..." required />
             <button type="submit"
                 class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
@@ -30,6 +30,9 @@
 
 <div class="grid grid-cols-1 p-5 mx-6 ">
 
+    @foreach ($penggunas as $pengguna )
+
+    @endforeach
     <table class="font-medium border border-black table-fixed">
         <thead class="bg-[#95AFE5]">
             <tr>
@@ -41,28 +44,34 @@
                 <th class="p-3 text-sm border border-black">Aksi</th>
             </tr>
         </thead>
+
+        @foreach ($penggunas as $pengguna )
         <tbody class="text-center">
             <tr>
-                <td class="p-3 text-sm border border-black">1</td>
-                <td class="p-3 text-sm border border-black">Lorem Epsum Sikocak Siolhok</td>
+                <td class="p-3 text-sm border border-black">{{ ($penggunas->currentPage() - 1) * $penggunas->perPage() + $loop->iteration }}</td>
+                <td class="p-3 text-sm border border-black">{{ $pengguna->nama }}</td>
                 <!-- <td class="p-3 text-sm border border-black">email@gmail.com</td>
                     <td class="p-3 text-sm border border-black">Mobil</td> -->
-                <td class="p-3 text-sm border border-black">BP 7896 BM</td>
-                <td class="p-3 text-sm border border-black">
-                    <button data-modal-target="small-modal" data-modal-toggle="small-modal"
+                    <td class="p-3 text-sm border border-black">{{ $pengguna->no_plat }}</td>
+                    <td class="p-3 text-sm border border-black">
+                        <button data-modal-target="small-modal" data-modal-toggle="small-modal"
                         class="px-2 py-1 text-xs bg-red-400 rounded-lg hover:bg-blue-200">Detail</button>
-                    <button data-modal-target="hapus-modal" data-modal-toggle="hapus-modal"
+                        <button data-modal-target="hapus-modal" data-modal-toggle="hapus-modal"
                         class="px-2 py-1 text-xs bg-red-400 rounded-lg hover:bg-blue-200">Hapus</button>
-                </td>
-            </tr>
-        </tbody>
+                    </td>
+                </tr>
+            </tbody>
+            @endforeach
     </table>
+</div>
+<div class="mt-4">
+    {{ $penggunas->links('pagination::tailwind') }}
 </div>
 
 <!-- Main Modal hapus slot -->
 <div id="hapus-modal" tabindex="-1"
     class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
-    <div class="relative p-4 w-full max-w-md max-h-full">
+    <div class="relative w-full max-w-md max-h-full p-4">
         <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
             <button type="button"
                 class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
@@ -74,8 +83,8 @@
                 </svg>
                 <span class="sr-only">Close modal</span>
             </button>
-            <div class="p-4 md:p-5 text-center">
-                <svg class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" aria-hidden="true"
+            <div class="p-4 text-center md:p-5">
+                <svg class="w-12 h-12 mx-auto mb-4 text-gray-400 dark:text-gray-200" aria-hidden="true"
                     xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                         d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
@@ -100,12 +109,12 @@
         <!-- Modal content -->
         <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
             <!-- Modal header -->
-            <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+            <div class="flex items-center justify-between p-4 border-b rounded-t md:p-5 dark:border-gray-600">
                 <h3 class="text-xl font-medium text-gray-900 dark:text-white">
                     Data Diri & Kendaraan Pengguna
                 </h3>
                 <button type="button"
-                    class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                    class="inline-flex items-center justify-center w-8 h-8 text-sm text-gray-400 bg-transparent rounded-lg hover:bg-gray-200 hover:text-gray-900 ms-auto dark:hover:bg-gray-600 dark:hover:text-white"
                     data-modal-hide="small-modal">
                     <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
                         viewBox="0 0 14 14">
@@ -116,9 +125,9 @@
                 </button>
             </div>
             <!-- Modal body -->
-            <div class="p-4 md:p-5 flex space-x-4 items-center">
+            <div class="flex items-center p-4 space-x-4 md:p-5">
                 <!-- Profile Image -->
-                <img src="{{ asset('img/user.jpg') }}" alt="User Profile" class="w-48 h-56 rounded-md object-cover -pt-5">
+                <img src="{{ asset('img/user.jpg') }}" alt="User Profile" class="object-cover w-48 h-56 rounded-md -pt-5">
 
                 <!-- User Information -->
                 <div class="space-y-1">
@@ -136,7 +145,7 @@
                 </div>
             </div>
             <!-- Modal footer
-            <div class="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
+            <div class="flex items-center p-4 border-t border-gray-200 rounded-b md:p-5 dark:border-gray-600">
                 <button data-modal-hide="small-modal" type="button"
                     class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">I
                     accept</button>
