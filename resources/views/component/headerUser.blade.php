ed8ab1b5a648fc627f981f869576faa5c7620e02
Adib-F <fakhrimuhammadadib@gmail.com>
2024-11-17 07:07:35 +0700

    Update: logout functionality on admin and user page

--- resources/views/component/headerUser.blade.php
+++ resources/views/component/headerUser.blade.php
@@ -1,16 +1,19 @@
-<nav class="bg-blue-600 text-white p-4 relative z-50">
-    <div class="container mx-auto flex justify-between items-center">
-        <h1 class="text-base font-bold pl-2">Hallo, Selamat Pagi</h1>
+<nav class="relative z-50 p-4 text-white bg-blue-600">
+    <div class="container flex items-center justify-between mx-auto">
+        <h1 class="pl-2 text-base font-bold">Hallo, Selamat Pagi</h1>
         <div class="dropdown-wrapper">
             <div class="flex items-center cursor-pointer" id="profileDropdown">
                 <span class="mr-2 text-sm">Nama Pengguna</span>
-                <div class="w-10 h-10 bg-gray-300 rounded-full flex items-center justify-center">
+                <div class="flex items-center justify-center w-10 h-10 bg-gray-300 rounded-full">
                     <i class="fas fa-user"></i>
                 </div>
             </div>
-            <div class="dropdown-menu mt-2 w-48 bg-white rounded-md shadow-lg hidden" id="dropdownMenu">
+            <div class="hidden w-48 mt-2 bg-white rounded-md shadow-lg dropdown-menu" id="dropdownMenu">
                 <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Ganti Password</a>
-                <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Logout</a>
+                <form action="{{ route('logout')}}" method="POST">
+                    @csrf
+                    <button type="submit" class="block px-4 text-sm text-gray-700 hover:bg-gray-100">Keluar</button>
+                </form>
             </div>
         </div>
     </div>
