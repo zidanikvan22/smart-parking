08ca190187dd615e12358cc89cf699eb84be0353
Adib-F <fakhrimuhammadadib@gmail.com>
2024-11-24 20:09:00 +0700

    Update: migration User

--- database/migrations/2024_11_05_100111_create_user_colection.php
+++ database/migrations/2024_11_05_100111_create_user_colection.php
@@ -13,14 +13,19 @@ return new class extends Migration
     {
         Schema::create('pengguna', function (Blueprint $table) {
             $table->id('id_pengguna');
-            $table->string('nama');
+            $table->string('identitas')->unique();
             $table->string('email')->unique();
-            $table->enum('jenis_kendaraan', ['mobil' , 'motor']);
-            $table->string('qr_code')->nullable();
-            $table->string('no_plat');
-            $table->string('foto_profile');
+            $table->string('nomor_telepon', 16)->unique();
+            $table->enum('jenis_pengguna', ['mahasiswa' , 'dosen' , 'karyawan' , 'tamu']);
+            $table->string('nama');
             $table->string('password');
+            $table->enum('jenis_kendaraan', ['mobil' , 'motor']);
+            $table->string('no_plat')->unique();
+            $table->string('foto_kendaraan');
+            $table->string('foto_pengguna');
+            $table->string('qr_code')->nullable();
             $table->enum('role', ['admin', 'pengguna'])->default('pengguna');
+            $table->enum('status', ['aktif', 'nonAktif', 'ditolak'])->default('nonAktif');
         });
     }
 
