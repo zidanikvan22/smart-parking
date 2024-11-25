55acd750070d6cae18314ffbcf2521df7a733444
Adib-F <fakhrimuhammadadib@gmail.com>
2024-11-25 08:41:01 +0700

    Update: registrasi

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -52,54 +52,59 @@ class AuthController extends Controller
     }
 
     public function registrasi_proses(Request $request)
-{
-    $validated = $request->validate([
-        'identitas' => 'required',
-        'jenis_pengguna' => 'required',
-        'email' => 'required|unique:pengguna|email',
-        'nomor_telepon' => 'required',
-        'nama' => 'required',
-        'password' => 'required',
-        'jenis_kendaraan' => 'required',
-        'no_plat' => 'required',
-        'foto_kendaraan' => 'required|image|file|mimes:jpeg,png,jpg',
-        'foto_pengguna' => 'required|image|file|mimes:jpeg,png,jpg',
-    ], [
-        'identitas.required' => 'Identitas wajib diisi',
-        'jenis_pengguna.required' => 'Jenis pengguna wajib diisi',
-        'nama.required' => 'Nama Lengkap harus diisi',
-        'email.required' => 'Email harus diisi',
-        'email.unique' => 'Email sudah terdaftar',
-        'email.email' => 'Email harus diisi dengan format email',
-        'nomor_telepon.required' => 'Nomor telepon wajib diisi',
-        'password.required' => 'Password wajib diisi',
-        'jenis_kendaraan.required' => 'Jenis kendaraan harus diisi',
-        'no_plat.required' => 'No Plat harus diisi',
-        'foto_kendaraan.required' => 'Foto kendaraan harus diisi',
-        'foto_kendaraan.mimes' => 'Foto kendaraan tidak sesuai format yang di tentukan',
-        'foto_pengguna.required' => 'Foto pengguna harus diisi',
-        'foto_pengguna.mimes' => 'Foto pengguna tidak sesuai dengan format yang di tentukan'
-    ]);
+    {
+        $validated = $request->validate([
+            'identitas' => 'required',
+            'jenis_pengguna' => 'required',
+            'email' => 'required|unique:pengguna|email',
+            'nomor_telepon' => 'required',
+            'nama' => 'required',
+            'password' => 'required',
+            'jenis_kendaraan' => 'required',
+            'no_plat' => 'required',
+            'foto_kendaraan' => 'required|image|file|mimes:jpeg,png,jpg',
+            'foto_pengguna' => 'required|image|file|mimes:jpeg,png,jpg',
+        ], [
+            'identitas.required' => 'Identitas wajib diisi',
+            'jenis_pengguna.required' => 'Jenis pengguna wajib diisi',
+            'nama.required' => 'Nama Lengkap harus diisi',
+            'email.required' => 'Email harus diisi',
+            'email.unique' => 'Email sudah terdaftar',
+            'email.email' => 'Email harus diisi dengan format email',
+            'nomor_telepon.required' => 'Nomor telepon wajib diisi',
+            'password.required' => 'Password wajib diisi',
+            'jenis_kendaraan.required' => 'Jenis kendaraan harus diisi',
+            'no_plat.required' => 'No Plat harus diisi',
+            'foto_kendaraan.required' => 'Foto kendaraan harus diisi',
+            'foto_kendaraan.mimes' => 'Foto kendaraan tidak sesuai format yang di tentukan',
+            'foto_pengguna.required' => 'Foto pengguna harus diisi',
+            'foto_pengguna.mimes' => 'Foto pengguna tidak sesuai dengan format yang di tentukan'
+        ]);
 
-    $foto_kendaraan = $request->file('foto_kendaraan')->store('image/fotoKendaraan', 'public');
-    $foto_pengguna = $request->file('foto_pengguna')->store('image/fotoPengguna', 'public');
+        $foto_kendaraan = $request->file('foto_kendaraan')->store('image/fotoKendaraan', 'public');
+        $foto_pengguna = $request->file('foto_pengguna')->store('image/fotoPengguna', 'public');
 
-    $store = [
-        'identitas' => $validated['identitas'],
-        'email' => $validated['email'],
-        'nomor_telepon' => $validated['nomor_telepon'],
-        'nama' => $validated['nama'],
-        'jenis_pengguna' => $validated['jenis_pengguna'],
-        'jenis_kendaraan' => $validated['jenis_kendaraan'],
-        'no_plat' => $validated['no_plat'],
-        'password' => Hash::make($validated['password']),
-        'foto_pengguna' => $foto_pengguna,
-        'foto_kendaraan' => $foto_kendaraan,
-    ];
+        $store = [
+            'identitas' => $validated['identitas'],
+            'email' => $validated['email'],
+            'nomor_telepon' => $validated['nomor_telepon'],
+            'nama' => $validated['nama'],
+            'jenis_pengguna' => $validated['jenis_pengguna'],
+            'jenis_kendaraan' => $validated['jenis_kendaraan'],
+            'no_plat' => $validated['no_plat'],
+            'password' => Hash::make($validated['password']),
+            'foto_pengguna' => $foto_pengguna,
+            'foto_kendaraan' => $foto_kendaraan,
+        ];
 
-    User::create($store);
-    return redirect()->route('login')->with('berhasil', 'Pendaftaran Berhasil');
-}
+        $user = User::create($store);
+        if ($user) {
+            return redirect()->route('login')->with('success', 'Pendaftaran berhasil');
+        } else {
+            return back()->with('error', 'Pendaftaran gagal');
+        }
+
+    }
 
 
     public function logout(Request $request)
