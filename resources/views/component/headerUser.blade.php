f788d4d7473ff36681ef15d4b26c88f2ccd6f187
Adib-F <fakhrimuhammadadib@gmail.com>
2024-11-27 15:38:03 +0700

    Fix code to align with expected behavior

--- resources/views/component/headerUser.blade.php
+++ resources/views/component/headerUser.blade.php
@@ -5,18 +5,22 @@
             <div class="flex items-center cursor-pointer" id="profileDropdown" onclick="toggleDropdown()">
                 <span class="mr-2 text-xl">{{ auth()->user()->nama }}</span>
                 <div class="flex items-center justify-center w-10 bg-gray-300 rounded-full">
-                    <img src="{{ Storage::url(auth()->user()->foto_profile) }}" alt="Profile Picture" class="object-cover w-full h-full rounded-full">
+                    <img src="{{ Storage::url(auth()->user()->foto_pengguna) }}" alt="Profile Picture"
+                        class="object-cover w-full h-full rounded-full">
                 </div>
             </div>
-            <div id="dropdownMenu" class="absolute right-0 z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow-md w-44 dark:bg-gray-700 dark:divide-gray-600">
+            <div id="dropdownMenu"
+                class="absolute right-0 z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow-md w-44 dark:bg-gray-700 dark:divide-gray-600">
                 <ul class="py-2 text-sm text-gray-700 dark:text-gray-200">
                     <li>
-                        <a href="{{ route('ubah-sandi') }}" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Profile</a>
+                        <a href="{{ route('ubah-sandi') }}"
+                            class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Profile</a>
                     </li>
                     <li>
                         <form action="{{ route('logout') }}" method="POST">
                             @csrf
-                            <button type="submit" class="block w-full px-4 py-2 text-start hover:bg-gray-100 dark:hover:bg-gray-600">Keluar</button>
+                            <button type="submit"
+                                class="block w-full px-4 py-2 text-start hover:bg-gray-100 dark:hover:bg-gray-600">Keluar</button>
                         </form>
                     </li>
                 </ul>
@@ -26,7 +30,7 @@
 </nav>
 
 <script>
-    document.addEventListener("DOMContentLoaded", function () {
+    document.addEventListener("DOMContentLoaded", function() {
         const greetingElement = document.getElementById("greeting");
         const currentHour = new Date().getHours();
 
@@ -43,5 +47,4 @@
 
         greetingElement.textContent = greetingText;
     });
-
 </script>
